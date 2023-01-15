d9e7b61e54a6377df4e26238eda053bacd39519e
Karthik Raja <karthikrajaamd@gmail.com>
2023-01-15 12:43:36 +0530

    apiurl

--- src/environment.js
+++ src/environment.js
@@ -1,5 +1,5 @@
 export const env = {
   // apiurl: "https://raja-crm.herokuapp.com",
-  apiurl: "http://localhost:8000",
-  // apiurl: "https://order-management-be.onrender.com",
+  // apiurl: "http://localhost:8000",
+  apiurl: "https://order-management-be.onrender.com",
 };
